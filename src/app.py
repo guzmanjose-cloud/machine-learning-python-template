e914e41dd47e42e63cbd36d04d121a031e7e96a4
Jose Antonio Guzman <guzmanjose639@yahoo.com>
2024-06-25 00:44:39 +0000

    explore.ipynb

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
-from utils import db_connect
-engine = db_connect()
+#from utils import db_connect
+#engine = db_connect()
 
-# your code here
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+file_path = "../data/raw/AB_NYC_2019.csv"
+data = pd.read_csv(file_path)
+data.head()
